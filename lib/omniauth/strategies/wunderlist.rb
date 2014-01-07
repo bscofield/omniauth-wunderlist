4498e0a87194264d6dea3a180f57a8b30809e611
Ben Scofield <git@turrean.com>
2014-01-07 13:37:01 +0100

    Fix headers for token and info retrieval

--- lib/omniauth/strategies/wunderlist.rb
+++ lib/omniauth/strategies/wunderlist.rb
@@ -4,25 +4,46 @@ module OmniAuth
   module Strategies
     class Wunderlist < OmniAuth::Strategies::OAuth2
       option :name, 'wunderlist'
+      option :provider_ignores_state, true
 
       option :client_options, {
         site: "https://a.wunderlist.com/api",
         authorize_url: "https://provider.wunderlist.com/login/oauth/authorize",
-        token_url: "https://provider.wunderlist.com/login/oauth/access_token"
+        token_url: "https://provider.wunderlist.com/login/oauth/access_token",
+        connection_opts: {
+          headers: {
+            'Accept' => 'application/json',
+            'Content-Type' => 'application/json',
+          }
+        }
       }
+
+      def request_phase
+        redirect client.auth_code.authorize_url
+      end
 
       uid { raw_info['id'] }
 
+      info do
+        {
+          email: raw_info['email'],
+          name: raw_info['name']
+        }
+      end
+
       extra do
-        {:raw_info => raw_info}
+        { raw_info: raw_info }
       end
 
       def raw_info
         access_token.options[:mode] = :query
-        access_token.options[:param_name] = :access_token
-        @raw_info ||= access_token.get('/1/user.json').parsed
+        @raw_info ||= access_token.get('/api/v1/user', {
+          headers: {
+            'X-Client-ID' => ENV['CLIENT_ID'],
+            'X-Access-Token' => access_token.token
+          }
+        }).parsed
       end
-
     end
   end
 end
